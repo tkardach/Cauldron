ed1ecd573307faad0dd1519a56a9333a7a5745f2
tkardach <tommy@kardach.com>
2025-10-02 16:15:36 -0700

    Remove old codebase

--- app/cauldron.py
+++
@@ -1,9 +0,0 @@
-import board
-from led.led_effect import SineWaveEffect
-import neopixel
-
-class BubblingEffect(SineWaveEffect):
-  """Simulates a bubbling affect on an LedStrip."""
-
-  def __init__(self, bubbling_speed: float = 0.5):
-    self.bubbling_speed = 1
